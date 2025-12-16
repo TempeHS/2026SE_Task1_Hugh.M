d79c88190e9c52d9212e8ff0ba2974f342e2080e
Hughie M <hughiemck@icloud.com>
2025-12-16 00:06:48 +0000

    wip: adding data to the base

--- userManagement.py
+++ userManagement.py
@@ -1,12 +1,30 @@
 import sqlite3 as sql
 import bcrypt
 
+connection = sql.connect("databaseFiles/database.db")
+cursor = connection.cursor()
 
-### example
+
+# test1 = input("un ")
+# test2 = input("pw ")
+
+
+# cursor.execute(
+#    f"SELECT * FROM user_data WHERE username = '{test1}' AND password = '{test2}'"
+# )
+
+# credentials = cursor.fetchall()
+
+
+# print(credentials)
+
+# connection.close()
+
+
+# example
 def getUsers():
     con = sql.connect("databaseFiles/database.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM id7-tusers")
     con.close()
     return cur
-
